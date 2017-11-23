e4afba9f520f24e3231bea18fd8291fc6253c7fd
unknown <samuel.genois@gmail.com>
2017-11-23 02:20:18 -0500

    Faster and simpler graph generator based on networkx. Heuristic being debugged

--- network.py
+++ network.py
@@ -1,144 +1,125 @@
-import random
 from functools import reduce
+from math import sqrt
+from random import choice
+from random import random
+from time import time as time
 
-#Creates a topology describing the connections between autonomous systems
-#This topology has a tree like structure
-def createTopology(size):
-
-    #Generate a tree
-    def createTree(a, b):
-
-        tree = [a]
-
-        nextNodeToPlace = a+1
-        while nextNodeToPlace < b:
-            lastSubtreeNode = random.randint(nextNodeToPlace, b-1)
-            tree.append(
-                createTree(nextNodeToPlace, lastSubtreeNode+1))
-            nextNodeToPlace = lastSubtreeNode + 1
-
-        return tree
-
-    tree = createTree(0,size)
-    
-    #Flatten the tree into a dictionary 
-    def flattenTree(graph, tree):
-        graph[tree[0]] = []
-        for branch in tree[1:]:
-            graph[tree[0]].append(branch[0])
-            flattenTree(graph, branch)
-
-    topology = {}
-
-    flattenTree(topology, tree)
-
-    #Make edges bidirectional
-    for asA in list(topology.keys()):
-        for asB in list(topology.keys())[asA:]:
-            if asB in topology[asA]:
-                topology[asB].append(asA)
-
-    #Return topology
-    return topology
-
-#Adds an undirected edge (as two directed edges) with a random weight between nodeA and nodeB.
-def add_edge(net, nodeA, nodeB):
-    weight = random.randint(1,10)
-    net[nodeA].append([nodeB, weight])
-    net[nodeB].append([nodeA, weight])
-
-#Implementation of the combination math operation
-def ncr(n, r):
-    r = min(r, n-r)
-    if r == 0: return 1
-    numer = reduce((lambda x, y: x * y), range(n, n-r, -1), 1)
-    denom = reduce((lambda x, y: x * y), range(1, r+1), 1)
-    return numer//denom
-
-#Returns the probability of having (at least) an edge between nodeA and nodeB
-def edge_prob(nodeADegree, nodeBDegree, totalDegree):
-    n2 = totalDegree - nodeADegree
-    n1 = n2 - nodeBDegree
-    r = nodeADegree
-    return 1 - ncr(n1, r) / ncr(n2, r)
-
-#Greate an autonomous system (a.k.a a sub network)
-def createAS(nodeIDStart, size):
-
-    _as = {}
-    degrees = []
-    totalDegree = 0
-
-    #Add nodes and generate an expected cardinality for each node
-    for node in range(nodeIDStart, nodeIDStart + size):
-        _as[node] = []
-        nodeDegree = random.randint(size//4, 3*size//4)
-        degrees.append((node, nodeDegree));
-        totalDegree += nodeDegree
-
-    #Add edges randomly based on the expected cardinality of each node
-    for nodeX in degrees:
-        for nodeY in degrees[degrees.index(nodeX)+1:]:
-            if random.random() <= edge_prob(nodeX[1], nodeY[1], totalDegree):
-                add_edge(_as, nodeX[0], nodeY[0])
-
-    return _as
-
-#Connects a group of autonomous systems as described by the topology
-#Returns a bundle of information regarding the create network, including the network itself
-def connectAS(topology, asList):
-
-    net = {}
-    nodeToASMap = {}
-    interASEdgeList = []
-
-    for _as in range(0, len(asList)):
-        for node in list(asList[_as].keys()):
-            net[node] = asList[_as][node]
-            nodeToASMap[node] = _as
-
-    asIndexList = list(topology.keys())
-    for _as in asIndexList:
-        for neighbor in topology[_as]:
-            if neighbor > _as:
-                nodeA = random.choice(list(asList[_as].keys()))
-                nodeB = random.choice(list(asList[neighbor].keys()))
-                interASEdgeList.append({
-                    'asA': _as,
-                    'asB': neighbor,
-                    'nodeA': nodeA,
-                    'nodeB': nodeB
-                })
-                add_edge(net, nodeA, nodeB)
-
-    return {'network' : net, 'nodeToASMap' : nodeToASMap, 'interASEdgeList' : interASEdgeList}
+import dill
+import networkx as nx
+import numpy as np
+import sys
 
 #Creates an entire network of the specified size
-def create(size):
+def create(size, randomness):
 
-    MIN_NUM_AS = 4
-    MAX_NUM_AS = 10
+    start_time = time()
 
-    nextNodeToCreate = 0
-    asList = []
+    num_as = int(sqrt(size))
 
-    while nextNodeToCreate < size:
+    as_boundaries = list(np.unique(np.random.randint(1, size, num_as)))
+    as_boundaries.insert(0,0)
+    as_boundaries.append(size)
 
-        newASSize = random.randint(
-            min(size - nextNodeToCreate,
-                size // MAX_NUM_AS),
-            min(size - nextNodeToCreate,
-                size // MIN_NUM_AS))
+    #Greate an autonomous system (a.k.a a sub network)
+    def create_as(start, end): 
 
-        asList.append(createAS(nextNodeToCreate, newASSize))
-        nextNodeToCreate += newASSize
+        print('Generating AS with nodes ' + str(start) + ' to ' + str(end-1) + '...')
 
-    topology = createTopology(len(asList))
+        m = randomness
 
+        # select some edge destinations
+        L = np.random.choice(range(start, end), 2*m)
+        # and suppose that each edge has a weight
+        weights = 0.5 + 5 * np.random.rand(m)
+
+        # create a graph object, add nodes to it, and the edges
+        G = nx.DiGraph()
+        G.add_nodes_from(range(start, end))
+        for i, (fr, to) in enumerate(zip(L[1::2], L[::2])):
+            if fr != to:
+                G.add_edge(fr, to, weight=np.around(weights[i])+1)
+        return G
+
+    as_list = list(map((lambda x : create_as(x[0], x[1])), zip(as_boundaries, as_boundaries[1:])))
+
+    print('Generating AS topology...')
+    as_topology = nx.random_tree(len(as_list))
+
+    print('Connecting AS...')
+    network = reduce((lambda x, y : nx.compose(x,y)), as_list)
+
+    msp = {}
+    get_as = {}
+    edge_node = {}
+    next_as = {}
     
-    netData = connectAS(topology, asList)
-    netData['topology'] = topology
-    netData['asList'] = asList
-    
-    return netData
 
+    for as_id in range(len(as_list)):
+        edge_node[as_id] = {}
+        next_as[as_id] = {}
+
+    for as1_id in range(len(as_list)):
+
+        as1 = as_list[as1_id]
+
+        for node in list(as1.nodes().keys()):
+            get_as[node] = as1_id
+
+        for as2_id in as_topology[as1_id]:
+
+            as2 = as_list[as2_id]
+
+            #Choose a node from first AS
+            as1_edge_node = choice(list(as1.nodes().keys()))
+
+            #Choose a node from first AS
+            as2_edge_node = choice(list(as2.nodes().keys()))
+
+            #Generate a random weight
+            network.add_edge(as1_edge_node, as2_edge_node, weight=0.5 + 5 * random())
+
+            msp[as1_edge_node] = nx.shortest_path(as1,source=as1_edge_node)
+            edge_node[as1_id][as2_id] = as1_edge_node
+            edge_node[as2_id][as1_id] = as2_edge_node
+
+        for as2_id in range(len(as_list))[as1_id+1:]:
+
+            as1_to_as2_path = nx.shortest_path(as_topology, as1_id, as2_id)
+            next_as[as1_id][as2_id] = as1_to_as2_path[1]
+            next_as[as2_id][as1_id] = as1_to_as2_path[-2]
+
+    def heuristic(node1, node2):
+        as1 = get_as[node1]
+        as2 = get_as[node2]
+        if as1 != as2:
+            return msp[edge_node[as1][next_as[as1][as2]]][node1] + msp[edge_node[as2][next_as[as2][as1]]][node2]
+        else:
+            return 0
+
+    runtime = time() - start_time
+    print("Graph generated in "+str(np.around(runtime, decimals=5))+" seconds!")
+
+   #Open output file
+    outfile = open(str(size) + '.network' ,'wb')
+
+    #Write network and heuristics to output file
+    dill.dump({'network' : network, 'heuristic' : heuristic}, outfile)
+
+    #Close output file
+    outfile.close()
+
+def load(size):
+    infile = open(str(size) + '.network' ,'rb')
+    network = dill.load(infile)
+    infile.close()
+    return network
+
+"""
+TODO make it not crash at this test
+create(20000, 30)
+net = load(20000)
+h = net['heuristic']
+print(h(0, 15))
+print(h(550, 3500))
+print(h(0, 19999))
+"""
