6312569716211e300e987bf03bc863b8eaaa288c
Bobak Hamed-Baghi <bobak.baghi@gmail.com>
2017-11-24 11:19:18 -0500

    Fixed dijkstra.py to use heap method, added unsused retracing function to it.

--- dijkstra.py
+++ dijkstra.py
@@ -37,26 +37,18 @@ def dijkstra(graph, source_node):
                 # now the predecessor is closest_node
                 previous_node[neighbour] = closest_node
                 # set the new distance on the tuple stored in heap
-
                 ##uncomment for linear method
                 unvisited[neighbour_index] = (new_distance, neighbour)
-
-                #uncomment for heap method
-                #new_node = (new_distance, neighbour)
-
                 # siftup the newly adjusted node until properly placed to preserve heap invariance
-
                 ## essentially linear method
-                # hq._siftup(unvisited, neighbour_index)
                 hq.heapify(unvisited)
-
                 ## heap method
-                #unvisited.pop(neighbour_index)
-                #hq.heappush(unvisited, new_node)
-                #print(unvisited)
+                # hq._siftdown(unvisited, 0, neighbour_index)
+            print(unvisited)
     return distance
 
-def targetted_dijkstra(graph, source_node,destination):
+
+def targetted_dijkstra(graph, source_node, destination):
     # initialize distance dict
     distance = {}
     # initialize previous node dict
@@ -90,11 +82,31 @@ def targetted_dijkstra(graph, source_node,destination):
                 # now the predecessor is closest_node
                 previous_node[neighbour] = closest_node
                 # set the new distance on the tuple stored in heap
-                # unvisited[neighbour_index] = (new_distance, neighbour)
-                new_node = (new_distance, neighbour)
+                ##uncomment for linear method
+                unvisited[neighbour_index] = (new_distance, neighbour)
                 # siftup the newly adjusted node until properly placed to preserve heap invariance
-                # hq._siftup(unvisited, neighbour_index)
-                # hq.heapify(unvisited)
-                unvisited.pop(neighbour_index)
-                hq.heappush(unvisited, new_node)
+                ## essentially linear method
+                hq.heapify(unvisited)
+                ## heap method
+                # hq._siftdown(unvisited, 0, neighbour_index)
+            print(unvisited)
     return distance
+
+
+# helper function that traces back through the previous nodes and compiles the steps needed to be taken
+# to get from source to destination.
+def _unpack_path_to_node(previous_node_dictionary, source_node, destination_node):
+    # last node checked in the chain
+    last_node = destination_node
+    # initialize empty path array
+    path = []
+    while (last_node != source_node):
+        # add last node to path
+        path.insert(0, last_node)
+        # update last node
+        last_node = previous_node_dictionary[last_node]
+        # check to see if there even is a connection
+        if last_node == None:
+            return None
+    # insert source node to the path
+    path.insert(0, source_node)
